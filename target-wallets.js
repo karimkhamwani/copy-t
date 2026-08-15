66cc094810a52f888dc80e8e096ed07b85b1092c
Karim Liaquat <karim.khamwani@hotmail.com>
2026-08-15 13:44:19 -0400

    Update target-wallets.js

--- target-wallets.js
+++ target-wallets.js
@@ -18,7 +18,7 @@ module.exports = [
   {
     address: "0xeebde7a0e019a63e6b476eb425505b7b3e6eba30",
     category: "crypto",
-    sub_category: ["btc-updown-5m"],
+    sub_category: ["eth-updown-5m"],
     max_trade_age_sec: 30, // 5m markets expire fast — don't chase old entries
   },
 ];
